10da64df4b28aa44951974fbc599a44c44a1ab17
Alexandra Lee <alexandraelee9@gmail.com>
2024-07-15 18:00:15 +0000

    session3 basic javaScript

---
+++ script.js
@@ -0,0 +1,18 @@
+//welcome
+function sayHello() {
+    const name = prompt("What is your name?")
+    alert("Hello " + name +", welcome to my website!");
+}
+sayHello()
+
+//dice role
+function rollDice() {
+    let dice = Math.floor(Math.random() * 6) + 1;
+    alert("You have rolled a " + dice + "!")
+}
+rollDice()
+
+//job search
+ function jobSearch() {
+
+ }
