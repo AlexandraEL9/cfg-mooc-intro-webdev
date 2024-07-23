c58c428ab39249fe914116fe7b7a17c7a152bc7b
Alexandra Lee <alexandraelee9@gmail.com>
2024-07-23 10:18:56 +0000

    JS and the DOM

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 //welcome
-function sayHello() {
-    const name = prompt("What is your name?")
-    alert("Hello " + name +", welcome to my website!");
-}
-sayHello()
+//function sayHello() {
+    //const name = prompt("What is your name?")
+    //alert("Hello " + name +", welcome to my website!");
+//}
+////sayHello()
 
 //dice role
-function rollDice() {
-    let dice = Math.floor(Math.random() * 6) + 1;
-    alert("You have rolled a " + dice + "!")
-}
-rollDice()
+//function rollDice() {
+    //let dice = Math.floor(Math.random() * 6) + 1;
+    //alert("You have rolled a " + dice + "!")
+//}
+//rollDice()
 
 //job search
- function jobSearch() {
+ //function jobSearch() {
 
+ //}
+
+ // object that has a movie
+
+ let myMovie = {
+    title: 'Avatar 2'
  }
